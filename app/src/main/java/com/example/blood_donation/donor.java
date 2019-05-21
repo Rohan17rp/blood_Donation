375c457bb6ff8a2f74ad289636536547ccf1bfee
Rohan <rohan17.rp@gmail.com>
2019-05-21 23:32:03 +0530

    select category modified classes and layouts to be continued

--- app/src/main/java/com/example/blood_donation/donor.java
+++ app/src/main/java/com/example/blood_donation/donor.java
@@ -2,6 +2,10 @@ package com.example.blood_donation;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class donor extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
@@ -9,3 +13,6 @@ public class donor extends AppCompatActivity {
         setContentView(R.layout.donor);
     }
 }
+
+
+
--- app/src/main/java/com/example/blood_donation/select_category.java
+++ app/src/main/java/com/example/blood_donation/select_category.java
@@ -1,11 +1,26 @@
 package com.example.blood_donation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class select_category extends AppCompatActivity {
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.category);
+        }
+
+    public void donor(View d){
+        Intent Donor = new Intent(this,donor.class);
+        startActivity(Donor);
+        select_category.this.finish();
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(this, login.class));
+        finish();
     }
 }
