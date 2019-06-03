8373454dee6b6a7605e100a03c942f36d1685c24
vedpatil611 <vedpatil611@gmail.com>
2019-06-03 15:10:24 +0530

    Minor fixes

--- app/src/main/java/com/rvDevelopers/BloodDonation/Profile.java
+++ app/src/main/java/com/rvDevelopers/BloodDonation/Profile.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
@@ -14,11 +15,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.Objects;
 
 public class Profile extends AppCompatActivity {
 
@@ -137,18 +135,23 @@ public class Profile extends AppCompatActivity {
     }*/
     @Override
     public void onBackPressed() {
-        new AlertDialog.Builder(this)
-                .setMessage("Are you sure you want to logout")
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        logout();
-                    }
-                })
-                .setNegativeButton("No", null)
-                .setCancelable(true)
-                .show();
+        if(dl.isDrawerOpen(GravityCompat.START)) {
+            dl.closeDrawer(GravityCompat.START);
+        } else {
+            new AlertDialog.Builder(this)
+                    .setMessage("Are you sure you want to logout")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            logout();
+                        }
+                    })
+                    .setNegativeButton("No", null)
+                    .setCancelable(true)
+                    .show();
+        }
     }
+
     public void logout() {
         startActivity(new Intent(this, MainActivity.class));
         finish();
